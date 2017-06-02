338b37e15812a34ef566e3b156bfe8dd9b765162
jayk1p <joukyuu@hotmail.com>
2017-06-01 17:13:35 -0700

    Create ToDoList.py

---
+++ ToDoList.py
@@ -0,0 +1,16 @@
+def run():
+
+    todo = []
+
+    raw = raw_input(" ")
+
+    if raw == True:
+        todo.append(raw)
+
+    if raw == "finished":
+        print todo
+
+
+    print todo
+
+run()
